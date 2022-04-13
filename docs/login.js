b0f0bcf5f7ed1063a63b2788c8a6e76816013d5d
smajwloo <smajw.vandeloo@student.avans.nl>
2022-04-13 11:24:52 +0200

    Added validation and minor changes

--- docs/login.js
+++ docs/login.js
@@ -1,12 +1,45 @@
-let usernameInput = document.querySelector('.username-container input')
-let submitButton = document.querySelector('.submit-button');
+const MAX_LENGTH = 30;
 
+let usernameInput = document.querySelector('.username-container input');
+let submitButton = document.querySelector('.submit-button');
+let validation = document.querySelector('.validation p');
+
+
+usernameInput.addEventListener('keyup', function() {
+    if(usernameInput.value.length > MAX_LENGTH) {
+        validateUsernameInput(undefined, "Username may contain only 30 characters");
+        showValidationMessage();
+    } else if (usernameInput.value.length <= MAX_LENGTH) {
+        hideValidationMessage();
+    }
+});
 
 submitButton.addEventListener('click', function() {
     let username = usernameInput.value;
+    let isUsernameValid = validateUsernameInput(username, "Please enter a valid username");
 
-    receiveUsername(username);
+    if (isUsernameValid) {
+        receiveUsername(username);
 
-    localStorage.setItem("username", username);
-    window.location = "index.html"
+        localStorage.setItem("username", username);
+        window.location = "snake.html";
+    }
 });
+
+function validateUsernameInput(usernameInputValue, errorMessage) {
+    if (!usernameInputValue || usernameInputValue.length > MAX_LENGTH) {
+        showValidationMessage();
+        validation.innerHTML = errorMessage;
+        return false;
+    }
+    hideValidationMessage();
+    return true;
+}
+
+function showValidationMessage() {
+    validation.style.visibility = 'visible';
+}
+
+function hideValidationMessage() {
+    validation.style.visibility = 'hidden';
+}
