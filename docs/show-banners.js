60d2e4386ed18ac111b2e4456bf9206a40eb1e69
smajwloo <smajw.vandeloo@student.avans.nl>
2022-04-21 16:32:27 +0200

    Added banners at login screen

---
+++ docs/show-banners.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    showBanners()
+};
+
+myBanners = [['Stichting \'t Oetske.png', 'https://showploegske.jouwweb.nl/'], ['taxibedrijfvdloo.png', 'https://taxibedrijfvdloo.jouwweb.nl/']];
+bannerIndex = 0;
+
+let banner = document.querySelector(".banner");
+
+function showBanners() { 
+    if (document.images) {
+        banner.removeEventListener("click", openBannerUrl);
+        bannerIndex++;
+
+        if (bannerIndex == myBanners.length) {
+            bannerIndex = 0;
+        }
+
+        banner.src = "./banners/" + myBanners[bannerIndex][0];
+        banner.addEventListener("click", openBannerUrl);
+
+        setTimeout("showBanners()", 5000);
+    }
+}
+
+function openBannerUrl() {
+    window.open(myBanners[bannerIndex][1]);
+}
